4e9485d07a683b52f08d8bd1b160a39862dddd81
Chris Mera <developer.chrismera@gmail.com>
2020-07-03 16:40:53 -0400

    installed apisauce library, created an api layer, and fetched listings data

--- app/components/Card.js
+++ app/components/Card.js
@@ -5,11 +5,11 @@ import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Text from "./Text";
 import colors from "../config/colors";
 
-const Card = ({ image, onPress, subTitle, title }) => {
+const Card = ({ imageUrl, onPress, subTitle, title }) => {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={image} />
+        <Image style={styles.image} source={{ uri: imageUrl }} />
         <View style={styles.detailsContainer}>
           <Text numberOfLines={1} style={styles.title}>
             {title}
